64db0d039c2eafbb8a172e9b39bc0828c6ac3cf7
David Wu <dwu@mobileiron.com>
2016-06-23 22:27:19 -0700

    asdf

--- index.js
+++ index.js
@@ -4,57 +4,24 @@ var _ = require('lodash');
 var childProcess = require('child_process')
 var fuzzy = require('fuzzy');
 
-
-// getBranches()
-// 	.then(getMatch)
-// 	.then(checkout)
-// 	.then(console.log.bind(null, 'checked out:'))
-// 	.catch(console.log.bind(null, 'error:'));
-
 currentBranch()
-	.then(unpushedLog)
-	.then(parseLog)
-	.then(console.log);
+	.then(unpushedCommits)
+	.then(parseCommits)
+	.then(modifyCommits)
+	.then(console.log)
+	.catch(console.log);
 
-
-// function getBranches(){
-// 	return new Promise(function(res, rej){
-// 		exec('git branch -a', {maxBuffer: 10 * 1024 * 1024}, function(err, stdout, stderr){
-// 			if(err){return rej(err);}
-
-// 			var branches = stdout.replace(/ /g, '').replace(/\*/g, '').split('\n');
-// 			branches = branches.filter(function(branch){
-// 				return branch.indexOf('->') === -1;
-// 			});
-// 			branches.pop();
-// 			return res(branches);
-// 		});
-// 	});
-// }
-
-// function getMatch(branches){
-// 	var result = fuzzy.filter(process.argv[2], branches)[0];
-// 	if(!result){
-// 		throw 'no match'
-// 	}
-// 	return result.string;
-// }
-
-
-// function checkout(branch){
-// 	return exec(`git checkout ${branch}`);
-// }
 
 function currentBranch(){
 	return exec('git name-rev --name-only HEAD');
 }
 
-function unpushedLog(branch){
+function unpushedCommits(branch){
 	return exec(`git log origin/${branch}..HEAD`);
 }
 
-function parseLog(str){
-
+function parseCommits(str){
+// console.log(str)
 	var re = /commit +([^\n]*)\nAuthor: +([^\n]*)\nDate: +([^\n]*?)\n/;
 	var arr = str.split(re);
 	arr.shift();
@@ -70,15 +37,15 @@ function parseLog(str){
 		}));
 	}
 
-	commits[0].setDate()
-	// _.reduce(commits, function(memo, commit){
-	// 	if(!memo){return commit.setDate()}
-	// 	memo.then(function(){
-	// 		return commit.setDate();
-	// 	})
-	// }, null);
-
 	return commits.reverse();
+}
+
+function modifyCommits(commits){
+
+	_.each(commits, function(commit){
+		commit.setDate();
+	})
+	return commits;
 }
 
 function exec(query, options){
@@ -101,25 +68,30 @@ function chomp(string){
 
 
 function Commit(options){
-	_.extend(this, options)
+	_.extend(this, options);
+	this.title = this.getTitle();
 }
 Commit.prototype = {
+	getTitle: function(){
+		return /\s*([^\s]*)/.exec(this.description)[1];
+	},
 	setDate: function(){
 		var id = this.id;
 
 		var query = `git filter-branch --env-filter \
-		    'if [ $GIT_COMMIT = ${id} ]
-		     then
-		         export GIT_COMMITTER_DATE="Sat May 19 01:01:01 2007 -0700"
-		     fi'`;
+			'if [ $GIT_COMMIT = ${id} ]
+				then
+					export GIT_AUTHOR_DATE='Sat, 14 Dec 2013 12:40:00 +0000'
+					export GIT_COMMITTER_DATE='Sat, 14 Dec 2013 12:40:00 +0000'
+			fi' && rm -fr "$(git rev-parse --git-dir)/refs/original/"`;
 
 		return exec(query)
-		.catch(function(err){
-			console.log('err',err)
-		})
-		.then(function(res){
-			console.log('succ', res)
-		})
+			.catch(function(err){
+				console.log('err',err)
+			})
+			.then(function(res){
+				console.log('succ', res)
+			})
 	}
 }
 
